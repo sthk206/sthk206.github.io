c14c03b2de84979f4f2e2187afa97b8f8b8ec40a
sthk206 <skaungse@ucsd.edu>
2021-12-20 03:02:10 -0800

    typing animation and font change

--- main.js
+++ main.js
@@ -1,22 +1,45 @@
 var i = 0;
-var txt = "Hi! My name is Si Thu.I'm an aspiring frontend developer with great interest in design.";
-var type = '<span id="myname">|</span>';
-var speed = 30; 
+var txt = "Hi, My name is Si Thu.I'm an aspiring frontend developer with great interest in design.";
+var speed = 50; 
+var pCount = 0;
 
 function typeW(){
   if (i < txt.length) {
-    if((txt.charAt(i) === '!' || txt.charAt(i) === '.') && i != txt.length-1){
-        document.getElementById("fullscreen-intro").innerHTML += txt.charAt(i);
+    if(( txt.charAt(i) === ',') && i != txt.length-1){
+      document.getElementById("fullscreen-intro").innerHTML += txt.charAt(i);
         document.getElementById("fullscreen-intro").innerHTML += "<br><br>";
+        i++;
+        pCount++;
+        setTimeout(typeW, speed);
+    }
+
+    else if(( txt.charAt(i) === '.') && i != txt.length-1){
+      document.getElementById("fullscreen-intro").innerHTML += txt.charAt(i);
+      setTimeout(function()
+      {
+        document.getElementById("fullscreen-intro").innerHTML += "<br>";
+
+      }, 650);
+      setTimeout(function()
+      {
+        document.getElementById("fullscreen-intro").innerHTML += "<br>";
+
+      }, 850);
+      setTimeout(function()
+      {
+        i++;
+        pCount++;
+        setTimeout(typeW, speed);
+      }, 1500);
+
     }
     else{
         document.getElementById("fullscreen-intro").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeW, pCount < 2? speed : speed/2);
     }
-    i++;
-    setTimeout(typeW, speed);
+
   }
-  else{
-    document.getElementById("fullscreen-intro").innerHTML += type;
-  }
+
 }
-window.onload=typeW();
+window.onload= setTimeout(typeW, 800); 
